a557eb6dda6e6a1215b3d3113358c100e64fc681
adrcle <adriclemenbot@gmail.com>
2024-01-30 16:18:27 +0100

    Commit:cambios2

--- src/main/java/com/example/primera_api_reset/repository/ResultsRepository.java
+++ src/main/java/com/example/primera_api_reset/repository/ResultsRepository.java
@@ -1,4 +1,14 @@
 package com.example.primera_api_reset.repository;
 
+import com.example.primera_api_reset.model.Circuit;
+import com.example.primera_api_reset.model.Race;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ResultsRepository {
+
+
+
+
 }
